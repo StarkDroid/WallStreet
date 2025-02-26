7aaa217e6d244e5744ff996aaac384e9a7a7e600
Trishiraj <gamerprince.exp@gmail.com>
2025-02-27 01:01:17 +0530

    Make the basics work on Android too

--- composeApp/src/androidMain/kotlin/com/velocity/wallstreet/ui/component/CardView.android.kt
+++ composeApp/src/androidMain/kotlin/com/velocity/wallstreet/ui/component/CardView.android.kt
@@ -1,8 +1,81 @@
 package com.velocity.wallstreet.ui.component
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
 import com.velocity.wallstreet.data.model.Desktop
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.resources.stringResource
+import wallstreet.composeapp.generated.resources.Res
+import wallstreet.composeapp.generated.resources.no_thumbnail_desc
+import wallstreet.composeapp.generated.resources.no_wallpaper
+import wallstreet.composeapp.generated.resources.wallpaper_thumbnail_desc
 
 @Composable
 actual fun CardView(wallpapers: Desktop) {
+    Card(
+        modifier = Modifier
+            .width(500.dp)
+            .height(250.dp)
+            .padding(8.dp)
+            .clickable {
+                if (wallpapers.imageUrl.isNotEmpty()) {
+
+                }
+            },
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 12.dp),
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            if (wallpapers.imageUrl.isEmpty()) {
+                Image(
+                    painter = painterResource(Res.drawable.no_wallpaper),
+                    contentDescription = stringResource(Res.string.no_thumbnail_desc),
+                    modifier = Modifier
+                        .size(100.dp)
+                        .align(Alignment.Center),
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                AsyncImage(
+                    model = wallpapers.imageUrl,
+                    modifier = Modifier.fillMaxSize(),
+                    contentScale = ContentScale.Crop,
+                    contentDescription = stringResource(Res.string.wallpaper_thumbnail_desc),
+                )
+            }
+
+            Box(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .background(
+                        color = MaterialTheme.colorScheme.tertiary,
+                        RoundedCornerShape(0.dp, 8.dp, 0.dp, 0.dp)
+                    )
+            ) {
+                Text(
+                    text = wallpapers.category,
+                    style = MaterialTheme.typography.bodyMedium,
+                    modifier = Modifier.padding(horizontal = 12.dp)
+                )
+            }
+        }
+    }
 }
--- composeApp/src/androidMain/kotlin/com/velocity/wallstreet/ui/component/GridView.android.kt
+++ composeApp/src/androidMain/kotlin/com/velocity/wallstreet/ui/component/GridView.android.kt
@@ -1,8 +1,46 @@
 package com.velocity.wallstreet.ui.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.foundation.lazy.grid.rememberLazyGridState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.velocity.wallstreet.data.model.Desktop
 
 @Composable
 actual fun GridView(wallpapers: List<Desktop>) {
+    val listState = rememberLazyGridState()
+
+    if (wallpapers.isNotEmpty()) {
+        Box(
+            modifier = Modifier.fillMaxSize()
+        ) {
+            LazyVerticalGrid(
+                modifier = Modifier.padding(16.dp),
+                state = listState,
+                columns = GridCells.Adaptive(minSize = 400.dp),
+                verticalArrangement = Arrangement.spacedBy(4.dp)
+            ) {
+                items(wallpapers) {
+                    CardView(it)
+                }
+            }
+        }
+    } else {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            LoadingIndicator()
+        }
+    }
 }
