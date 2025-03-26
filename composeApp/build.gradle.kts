6e336d86a0fc01c8375eb53462d2c6e3bb3bb122
Trishiraj <gamerprince.exp@gmail.com>
2025-03-26 22:21:26 +0530

    Introduce FCM notifications

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
     alias(libs.plugins.androidApplication)
     alias(libs.plugins.composeMultiplatform)
     alias(libs.plugins.composeCompiler)
+    id("com.google.gms.google-services")
     kotlin("plugin.serialization") version "2.1.10"
 }
 
@@ -28,6 +29,8 @@ kotlin {
             implementation(libs.ktor.client.android)
             implementation(libs.navigation.compose)
             implementation(libs.splashscreen.compose)
+            implementation(libs.firebase.messaging)
+            implementation(project.dependencies.platform(libs.firebase.bom))
         }
         commonMain.dependencies {
             implementation(compose.runtime)
--- composeApp/src/androidMain/kotlin/com/velocity/wallstreet/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/velocity/wallstreet/MainActivity.kt
@@ -4,16 +4,35 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.core.view.WindowCompat
 import com.velocity.wallstreet.navigation.WallStreetNavGraph
 import com.velocity.wallstreet.theme.AppTheme
+import com.velocity.wallstreet.utils.NotificationPermissionHandler
+import com.velocity.wallstreet.utils.createNotificationChannel
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var notificationPermissionHandler: NotificationPermissionHandler
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         enableEdgeToEdge()
+
+        notificationPermissionHandler = NotificationPermissionHandler(this)
+
+        val requestPermissionLauncher =
+            registerForActivityResult(ActivityResultContracts.RequestPermission()) {
+                isGranted -> notificationPermissionHandler.handlePermissionResult(isGranted)
+            }
+
+        notificationPermissionHandler.setRequestPermissionLauncher(requestPermissionLauncher)
+
+        notificationPermissionHandler.askNotificationPermission()
+
+        createNotificationChannel(this)
 
         val windowDecor = WindowCompat.getInsetsController(window, window.decorView)
 
