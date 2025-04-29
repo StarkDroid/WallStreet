83ebac5c84f2273c89195bddf33d682044e1653f
Trishiraj <gamerprince.exp@gmail.com>
2025-04-29 17:03:59 +0530

    Revert "Introduce FCM notifications"
    This reverts commit 6e336d86a0fc01c8375eb53462d2c6e3bb3bb122.

--- build.gradle.kts
+++ build.gradle.kts
@@ -6,5 +6,4 @@ plugins {
     alias(libs.plugins.composeMultiplatform) apply false
     alias(libs.plugins.composeCompiler) apply false
     alias(libs.plugins.kotlinMultiplatform) apply false
-    alias(libs.plugins.googleServices) apply false
 }
