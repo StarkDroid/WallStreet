b6d5e6375d8211aa88f3f68360474445df08af74
Trishiraj <gamerprince.exp@gmail.com>
2025-06-09 13:58:21 +0530

    Bump android version code to 6

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -70,7 +70,7 @@ android {
         applicationId = "com.velocity.wallstreet"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 4
+        versionCode = 6
         versionName = project.version.toString()
     }
     packaging {
